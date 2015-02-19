1723219f08a30a02f16900baff1ddf84b6e89c20
Flippy <flippy10@gmail.com>
2015-02-19 17:04:57 -0500

    Second Commit - Battery Monitoring
    Added battery level monitoring.

--- TeensyButtoner.ino
+++ TeensyButtoner.ino
@@ -11,6 +11,13 @@ const int pinBtnTrigLeft = 21;
 const int pinBtnTrigRight = 20;
 const int pinLEDOutput = 11;
 
+const int pinBattLEDOutput1 = 18;
+const int pinBattLEDOutput2 = 19;
+
+const int pinBattProbe = 17;
+
+bool low_batt = false;
+
 byte buttons[] = { 
   pinBtnUp,
   pinBtnRight,
@@ -59,6 +66,8 @@ void setup()
 {
   //Setup the pin modes.
   pinMode(pinLEDOutput, OUTPUT);
+  pinMode(pinBattLEDOutput1, OUTPUT);
+  pinMode(pinBattLEDOutput2, OUTPUT);
   for (byte i=0; i< NUMBUTTONS; i++) {
     /* We don't want INPUT_PULLUPs on a HIGH - ON LOW - OFF button. */
     if (!is_inverted[i]){
@@ -75,12 +84,41 @@ void setup()
 
 void loop()
 {
-  // //debugging the start button...
-  digitalWrite ( pinLEDOutput, digitalRead(pinBtnStart));
-  //Progess the SNES controller buttons to send keystrokes.
+  /* Uncomment this so the onboard LED is has direct feedback with the start button for an easy indication that everything
+  is working ok.*/
+  //digitalWrite ( pinLEDOutput, digitalRead(pinBtnStart));
   fcnProcessButtons();
+  fcnCheckBatteryLevel();
 }
 
+void fcnCheckBatteryLevel()
+{
+  int currentBatteryLevel = analogRead(pinBattProbe);
+  float batteryVoltage = currentBatteryLevel * (5.0 / 1023.0);
+  if (batteryVoltage < 3.3){
+     /* Battery floor cutoff is coming very soon... */  
+     batteryIndicator(true);
+  } else {
+      /* This could alternatively be done by reading the state of one of the LED output pins to determine
+      what the LED is currently doing, however the boolean is less cryptic. */
+      if (low_batt == true){
+          batteryIndicator(false);
+      }
+  }
+}
+
+void batteryIndicator(bool low_battery)
+{
+    if (low_battery){
+      digitalWrite(pinBattLEDOutput1, LOW);
+      digitalWrite(pinBattLEDOutput2, HIGH);
+      low_batt = true;
+    } else {
+      digitalWrite(pinBattLEDOutput1, HIGH);
+      digitalWrite(pinBattLEDOutput2, LOW);
+      low_batt = false;
+    }
+}
 
 //Function to process the buttons from the SNES controller
 void fcnProcessButtons()
@@ -111,7 +149,8 @@ void fcnProcessButtons()
 
 /* ButtonState - This will do a digital read with the proper logic for the input being polled.
 i is the index of the button being polled. */
-bool buttonState(int i){
+bool buttonState(int i)
+{
   if (!is_inverted[i]){
     /* Code to handle non-inverting (with respect to the INPUT PULLUP) buttons */
     if (! digitalRead(buttons[i])){
